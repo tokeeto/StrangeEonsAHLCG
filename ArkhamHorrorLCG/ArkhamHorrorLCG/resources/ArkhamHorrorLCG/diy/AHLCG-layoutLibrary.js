8f3a545f832c57921851e9c70e4830d8b6c625be
Ben Turner <ben.turner@pobox.com>
2026-07-03 17:39:58 +1000

    Use high-res PNG templates for dynamic card backs
    The static card settings already reference the high-resolution .png templates, but the runtime code that reassigns a card's back template still pointed at the legacy .jp2 files, silently downgrading those faces to low resolution.
    
    Add a guarded templateResource() helper so a template name without a hi-res PNG falls back to .jp2 instead of a missing file.

--- ArkhamHorrorLCG/ArkhamHorrorLCG/resources/ArkhamHorrorLCG/diy/AHLCG-layoutLibrary.js
+++ ArkhamHorrorLCG/ArkhamHorrorLCG/resources/ArkhamHorrorLCG/diy/AHLCG-layoutLibrary.js
@@ -892,10 +892,10 @@ function layoutBackTypeStats( diy, bindings, faceIndex ) {
 	BackList.addActionListener( function updateShape( actionEvent ) {
 		try {
 			if ( (String)(BackList.getSelectedItem()) == 'Player' ) {
-				diy.settings.set( 'AHLCG-' + CardTypes[1] + '-Default-template', 'ArkhamHorrorLCG/templates/AHLCG-PlayerBack.jp2' );
+				diy.settings.set( 'AHLCG-' + CardTypes[1] + '-Default-template', 'ArkhamHorrorLCG/templates/AHLCG-PlayerBack.png' );
 			}
 			else {
-				diy.settings.set( 'AHLCG-' + CardTypes[1] + '-Default-template', 'ArkhamHorrorLCG/templates/AHLCG-EncounterBack.jp2' );
+				diy.settings.set( 'AHLCG-' + CardTypes[1] + '-Default-template', 'ArkhamHorrorLCG/templates/AHLCG-EncounterBack.png' );
 			}
 		} catch (ex) {
 			Error.handleUncaught( ex );
@@ -904,7 +904,7 @@ function layoutBackTypeStats( diy, bindings, faceIndex ) {
 
 	StatsPanel.place(
 		@AHLCG-Back, 'align right ', BackList, 'wrap, width 40%'
-		);
+	);
 
 	return StatsPanel;
 }
@@ -919,13 +919,13 @@ function layoutLocationBackTypeStats( diy, bindings, faceIndex ) {
  	BackList.addActionListener( function updateShape( actionEvent ) {
 		try {
 			if ( (String)(BackList.getSelectedItem()) == 'Player' ) {
-				$AHLCG-LocationBack-Default-template = 'ArkhamHorrorLCG/templates/AHLCG-PlayerBack.jp2';
+				$AHLCG-LocationBack-Default-template = 'ArkhamHorrorLCG/templates/AHLCG-PlayerBack.png';
 			}
 			else if ( (String)(BackList.getSelectedItem()) == 'Encounter' ) {
-				$AHLCG-LocationBack-Default-template = 'ArkhamHorrorLCG/templates/AHLCG-EncounterBack.jp2';
+				$AHLCG-LocationBack-Default-template = 'ArkhamHorrorLCG/templates/AHLCG-EncounterBack.png';
 			}
 			else {
-				$AHLCG-LocationBack-Default-template = 'ArkhamHorrorLCG/templates/AHLCG-LocationBack.jp2';
+				$AHLCG-LocationBack-Default-template = 'ArkhamHorrorLCG/templates/AHLCG-LocationBack.png';
 			}
 		} catch (ex) {
 			Error.handleUncaught( ex );
